3f533414b76cbac01ef40d62d636a67be4a3ac3d
mauro arias <mauro.arias.a@gmail.com>
2022-03-13 19:03:46 -0400

    looking for th eproper syntax to load classes

--- src/org/mauro/LibLoader.groovy
+++ src/org/mauro/LibLoader.groovy
@@ -1,13 +1,15 @@
 package org.mauro
     
-def loadLib () {
-    def libVersion = 'wip-0.1.0'
-    sh "echo 'loading lib version: ${libVersion}'"
-    library identifier: "jenkins-share-lib@${libVersion}", retriever: 
-        modernSCM(
-            [$class: 'GitSCMSource',
-            remote: 'https://github.com/mauroarias/jenkins-share-lib.git'])
-    jenkinsLib.prepareLib()
-}
+public class LibLoader {
 
-return this
+    private static String libVersion = 'wip-0.1.0'
+
+    public static void loadLib () {
+        sh "echo 'loading lib version: ${libVersion}'"
+        library identifier: "jenkins-share-lib@${libVersion}", retriever: 
+            modernSCM(
+                [$class: 'GitSCMSource',
+                remote: 'https://github.com/mauroarias/jenkins-share-lib.git'])
+        jenkinsLib.prepareLib()
+    }
+}
