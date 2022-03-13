328d84d9d4591511732edb0e051b8ab2232138d1
mauro arias <mauro.arias.a@gmail.com>
2022-03-13 15:32:03 -0400

    removing to classes

--- src/org/mauro/LibLoader.groovy
+++ src/org/mauro/LibLoader.groovy
@@ -1,8 +1,7 @@
 package org.mauro
-
-def libVersion = 'wip-0.1.0'
     
 def loadLib () {
+    def libVersion = 'wip-0.1.0'
     sh "echo 'loading lib version: ${libVersion}'"
     library identifier: "jenkins-share-lib@${libVersion}", retriever: 
         modernSCM(
