283bf1beaf0a234ee908da87c54cc11f573a5bfc
mauro arias <mauro.arias.a@gmail.com>
2022-03-14 11:50:32 -0400

    new test

--- src/org/mauro/LibLoader.groovy
+++ src/org/mauro/LibLoader.groovy
@@ -1,8 +1,7 @@
 package org.mauro
     
-def libVersion = 'wip-0.1.0'
-
 def loadLib () {
+    libVersion = 'wip-0.1.0'
     sh "echo 'loading lib version: ${libVersion}'"
     library identifier: "jenkins-share-lib@${libVersion}", retriever: 
         modernSCM(
