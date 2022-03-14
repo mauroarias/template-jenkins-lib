8bd1ca8cdd5dd91ecf4131825b55c3acb27c6fb8
mauro arias <mauro.arias.a@gmail.com>
2022-03-14 11:47:39 -0400

    new test

--- src/org/mauro/LibLoader.groovy
+++ src/org/mauro/LibLoader.groovy
@@ -1,14 +1,13 @@
 package org.mauro
     
-class LibLoader implements Serializable {
+def libVersion = 'wip-0.1.0'
 
-    def private static String libVersion = 'wip-0.1.0'
-
-    def public static void loadLib () {
-        sh "echo 'loading lib version: ${libVersion}'"
-        library identifier: "jenkins-share-lib@${libVersion}", retriever: 
-            modernSCM(
-                [$class: 'GitSCMSource',
-                remote: 'https://github.com/mauroarias/jenkins-share-lib.git'])
-    }
+def loadLib () {
+    sh "echo 'loading lib version: ${libVersion}'"
+    library identifier: "jenkins-share-lib@${libVersion}", retriever: 
+        modernSCM(
+            [$class: 'GitSCMSource',
+            remote: 'https://github.com/mauroarias/jenkins-share-lib.git'])
 }
+
+return this
