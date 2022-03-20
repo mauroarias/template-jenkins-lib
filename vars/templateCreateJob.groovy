e3854d972e1a1c93280550f466dfae811b723f87
mauro arias <mauro.arias.a@gmail.com>
2022-03-20 07:50:10 -0400

    testing

--- vars/templateCreateJob.groovy
+++ vars/templateCreateJob.groovy
@@ -35,6 +35,7 @@ def call(body) {
                         sh "echo 'git remote: ${gitDstRemote}'"
                         sh "echo 'service name: ${serviceName}'"
                         sh "echo 'manual trigger: ${params.manualTrigger}'"
+                        jenkinsLib.configRunner(this)
                         gitLib.configGitRep("${gitDstRemote}")
                         gitLib.createProjectIfNotExitsIfAppl("${serviceName}")
                     }
